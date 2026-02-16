945c28724b93cc46a148713c059cb2813aa58edd
hrushikeshabehera <hrushikesha@cluenuts.com>
2026-02-16 18:07:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+// const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+// Connect database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // For parsing JSON request bodies
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully 🚀");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
